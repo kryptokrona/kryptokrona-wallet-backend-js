7fefa033040e5c54cf1b01319c8e9bb13b0cff78
zoidbergZA <5298218+zoidbergZA@users.noreply.github.com>
2019-05-18 20:14:25 +0200

    Update lib/WalletBackend.ts
    Co-Authored-By: zpalmtree <22151537+zpalmtree@users.noreply.github.com>

--- lib/WalletBackend.ts
+++ lib/WalletBackend.ts
@@ -1118,7 +1118,7 @@ export class WalletBackend extends EventEmitter {
     }
 
     /**
-     * encrypt the wallet using the given password. Password may be empty. Note that an empty password does not mean an
+     * Encrypt the wallet using the given password. Password may be empty. Note that an empty password does not mean an
      * unencrypted wallet - simply a wallet encrypted with the empty string.
      *
      * This will take some time (Roughly a second on a modern PC) - it runs 500,000 iterations of pbkdf2.
